4e0597b4ae32de916748ec3c3abf477647d2d6e2
denzakh <den.zakh@gmail.com>
2019-09-30 01:34:57 +0300

    Updates

---
+++ precache-manifest.9f0d566e8b1feeab3e59e602853c85a2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0679ab2b6763fcd30fe5ec1d2bf3bfed",
+    "url": "./index.html"
+  },
+  {
+    "revision": "19cb7c7c1c7b45890988",
+    "url": "./static/css/main.011fba78.chunk.css"
+  },
+  {
+    "revision": "eb2cc3b53e899c6297cb",
+    "url": "./static/js/2.21e6777b.chunk.js"
+  },
+  {
+    "revision": "19cb7c7c1c7b45890988",
+    "url": "./static/js/main.6250f3cf.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
